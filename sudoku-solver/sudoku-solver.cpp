a8937966adbe8588337c5a4d8ea62ed9f22cc864
Ajinkya Kadam <63292908+Ajink7@users.noreply.github.com>
2021-08-21 18:21:17 +0530

    Time: 32 ms (31.39%), Space: 6.5 MB (25.06%) - LeetHub

---
+++ sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool isValid(vector<vector<char>>& board, int x, int y, char c)
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            if(board[i][y] == c)
+                return false;
+            if(board[x][i] == c)
+                return false;
+            if(board[3*(x / 3) + i / 3][3*(y/3) + i % 3] == c)
+                return false;
+        }
+        return true;
+    }
+    
+    bool solve(vector<vector<char>>& v){
+        for(int i=0;i<v.size();i++)
+        {
+            for(int j=0;j<v[i].size();j++)
+            {
+                if(v[i][j]=='.')
+                {
+                    for(char k = '1';k<='9';k++)
+                    {
+                        
+                        if(isValid(v,i,j,k))
+                        {
+                            v[i][j] = k;
+                            if(solve(v))
+                                return true;
+                            else
+                                v[i][j] = '.';
+                        } 
+                        
+                    }
+                    return false;
+                }
+                
+            }
+        }
+        return true;
+    }
+    void solveSudoku(vector<vector<char>>& v) {
+        solve(v);
+    }
+};
